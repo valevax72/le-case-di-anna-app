08fad66cfea3e408b60b6db0ddf82c7627b97d75
Valerio <valerio.calvi@camngo.it>
2026-07-19 11:05:24 +0200

    Rimuove la revoca Google dal menu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'case-anna-2.4.0';
+const CACHE = 'case-anna-2.4.1';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
