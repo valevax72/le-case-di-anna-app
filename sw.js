2fbfe42ef6e879c55d02a76d2f41ac2812477465
Valerio <valerio.calvi@camngo.it>
2026-07-19 10:45:13 +0200

    Migliora accesso e uscita Google

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'case-anna-2.3.0';
+const CACHE = 'case-anna-2.4.0';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
