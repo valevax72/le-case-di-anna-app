5adfea152df13443d355a8af8144eeebeb3d8327
Valerio <valerio.calvi@camngo.it>
2026-07-19 13:13:31 +0200

    Aggiunge account attivo e notifiche revisioni

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'case-anna-2.4.1';
+const CACHE = 'case-anna-2.5.0';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
