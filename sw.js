8ace11df532a2bf3d6780e628bd9e6769e02d693
Valerio <valerio.calvi@camngo.it>
2026-07-19 19:43:43 +0200

    Corregge filtro gruppi di pagamento

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'case-anna-2.7.0';
+const CACHE = 'case-anna-2.7.1';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
